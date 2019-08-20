1e57c1a406e13b658f3b5ad986a7f71c6604fdd9
code-and-technology <tony.gottschalk@alumni.unc.edu>
2019-08-20 14:44:52 -0400

    Updates

---
+++ precache-manifest.112066321cb92b619389a5b724d78c55.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52d553222a717cb47128dc70c4ea8ac",
+    "url": "/react-hangman-game/index.html"
+  },
+  {
+    "revision": "cdec4a1869c62b241eed",
+    "url": "/react-hangman-game/static/css/main.397ceade.chunk.css"
+  },
+  {
+    "revision": "035a63ec5841945f68a7",
+    "url": "/react-hangman-game/static/js/2.2500a2f6.chunk.js"
+  },
+  {
+    "revision": "cdec4a1869c62b241eed",
+    "url": "/react-hangman-game/static/js/main.1dd6f1f0.chunk.js"
+  },
+  {
+    "revision": "1c26272d24b7ee47d4ec",
+    "url": "/react-hangman-game/static/js/runtime~main.05c52b62.js"
+  },
+  {
+    "revision": "932a05d32533ddf104c7e3f3874fcb7d",
+    "url": "/react-hangman-game/static/media/1.932a05d3.png"
+  },
+  {
+    "revision": "656e9ecedac92c1c29dd965822233e33",
+    "url": "/react-hangman-game/static/media/2.656e9ece.png"
+  },
+  {
+    "revision": "0d98c1db6d9ebb4087bedab1aa7cd468",
+    "url": "/react-hangman-game/static/media/3.0d98c1db.png"
+  },
+  {
+    "revision": "28889e2f8e16394f3aae7a5ff20045bb",
+    "url": "/react-hangman-game/static/media/4.28889e2f.png"
+  },
+  {
+    "revision": "0e949d9e0a6f5d6ae87501a8b1398747",
+    "url": "/react-hangman-game/static/media/5.0e949d9e.png"
+  },
+  {
+    "revision": "73eed85381a7229948dd912315b7940d",
+    "url": "/react-hangman-game/static/media/6.73eed853.png"
+  },
+  {
+    "revision": "d584fd827c80d18ee721a82e87782778",
+    "url": "/react-hangman-game/static/media/7.d584fd82.png"
+  }
+]);
